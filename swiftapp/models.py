d1ce1d72777015c557c985df8a53ab848121a352
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-04-27 21:47:00 +0100

    code optimized

--- swiftapp/models.py
+++ swiftapp/models.py
@@ -65,8 +65,8 @@ class SwiftUser(AbstractUser):
         return True
 
 
-class ShowElement(models.Model):
-    are_visible = models.BooleanField(default=False)
+#class ShowElement(models.Model):
+    #are_visible = models.BooleanField(default=False)
 
 class UserItems(models.Model):
 
